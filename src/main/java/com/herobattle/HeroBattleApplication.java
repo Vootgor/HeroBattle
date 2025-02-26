36f95628b750ec152070abf1774ef7486a06a2dc
Georgii Voronov <georgiivoronov@hollandandbarrett.com>
2025-02-26 19:16:35 +0100

    Initial commit

---
+++ src/main/java/com/herobattle/HeroBattleApplication.java
@@ -0,0 +1,13 @@
+package com.herobattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroBattleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeroBattleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/herobattle/HeroBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herobattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroBattleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
